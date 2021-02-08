6dc873c05e8a4db3ab39e941afe00c52aa85e8dd
Alistair Young <avatar@arkane-systems.net>
2021-02-08 09:30:15 -0600

    Most basic API controller.

---
+++ MongrelWiki/Controllers/Api/AppController.cs
@@ -0,0 +1,29 @@
+﻿#region header
+
+// MongrelWiki - AppController.cs
+// 
+// Created by: Alistair J R Young (avatar) at 2021/02/08 9:22 AM.
+
+#endregion
+
+#region using
+
+using System.Reflection;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+#endregion
+
+namespace ArkaneSystems.MongrelWiki.Controllers.Api
+{
+    [Route (template: "api/[controller]")]
+    [ApiController]
+    public class AppController : ControllerBase
+    {
+        // GET: /api/app/version
+        [HttpGet (template: "version")]
+        public async Task<ActionResult<string>> GetWikiVersion ()
+            => Assembly.GetExecutingAssembly ().GetName ().Version!.ToString ();
+    }
+}
